a53c90988965ac022955d1af43e5e51186441614
Paul Guevarra <p.a.guevarra@gmail.com>
2017-07-07 12:33:10 -0700

    add a second branch of code to narrow down suggestion response.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
   $("form#blanks").submit(function(event) {
     var devType = $("#platform").val();
+    var mySide = $("#side").val();
+    var myLike = $("#microsoft").val();
     event.preventDefault();
 
     if (devType === "game") {
@@ -11,6 +13,28 @@ $(document).ready(function() {
       $("#java").show();
       $("#cnet").hide();
       $("#css").hide();
+    } else if (devType === "web") {
+
+      if (mySide === "front") {
+        $("#java").hide();
+        $("#cnet").hide();
+        $("#css").show();
+      } else if (mySide === "back" && myLike === "love") {
+          $("#java").hide();
+          $("#cnet").show();
+          $("#css").hide();
+      } else if (mySide === "back" && myLike === "alright") {
+          $("#java").show();
+          $("#cnet").hide();
+          $("#css").hide();
+      } else if (mySide === "back" && myLike === "yuck") {
+          $("#java").show();
+          $("#cnet").hide();
+          $("#css").hide();
+      } else {
+        alert("Complete all fields before submitting");
+        }
+
     } else {
       alert("Complete all fields before submitting");
     }
